9ca43aa25ddeedc03b34c13b16d557163c472209
srdrabx <devalladolidoficial@gmail.com>
2022-07-11 19:09:57 +0200

    Switch to JS as TS doesnt work

---
+++ functions/add-message.js
@@ -0,0 +1,5 @@
+module.exports = (req, res) => {
+  res
+    .status(200)
+    .send(`Nhost, from Javascript, pays it's respects to ${req.query.name}!`);
+};
--- functions/add-message.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-
-import { Request, Response } from "express";
-export default (req: Request, res: Response) => {
-  res
-    .status(200)
-    .send(`Nhost, from Typescript, pays it's respects to ${req.query.name}!`);
-};
