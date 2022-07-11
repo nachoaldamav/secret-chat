60a572b560ef44997a56aaac76177c9603b866d9
srdrabx <devalladolidoficial@gmail.com>
2022-07-11 19:04:22 +0200

    Test functions

--- functions/add-message.ts
+++ functions/add-message.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
+
 export default function request(req: Request, res: Response) {
+  const body = req.body;
+  console.log(body);
   res
     .status(200)
     .send(`Nhost, from Typescript, pays it's respects to ${req.query.name}!`);
