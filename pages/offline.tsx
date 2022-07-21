b3e31f5437834c2c6643ddb143c4130c694ce58f
srdrabx <devalladolidoficial@gmail.com>
2022-07-21 09:52:02 +0200

    Modify offline page

--- pages/offline.tsx
+++ pages/offline.tsx
@@ -1,7 +1,7 @@
 export default function Offline() {
   return (
     <div className="h-full w-full inline-flex justify-center items-center">
-      <p className="text-4xl font-bold">You are offline</p>
+      <p className="text-4xl font-bold">No tienes conexión</p>
     </div>
   );
 }
